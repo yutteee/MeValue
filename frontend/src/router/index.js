996935c653687c0468362f0d792e5f75130cd682
yutteee <yusaku_nakamura_aa@icloud.com>
2022-04-15 17:55:16 +0900

    viewsファイル整理

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginPage from '../views/LoginPage.vue'
+import AboutMePage from '../views/AboutMePage.vue'
+import ContactPage from '../views/ContactPage.vue'
+import MyPage from '../views/MyPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: LoginPage
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'aboutme',
+    component: AboutMePage
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactPage
+  },
+  {
+    path: '/mypage',
+    name: 'mypage',
+    component: MyPage
   }
 ]
 

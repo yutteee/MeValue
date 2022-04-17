cdeda124199e9a65fa03c1e830824572a663726d
yutteee <yusaku_nakamura_aa@icloud.com>
2022-04-17 14:15:34 +0900

    ログインページ作った(仮)

---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,25 @@
+<template>
+<v-container>
+    <v-img
+        src="../assets/logo.png"
+        max-width="400"
+    />
+    <v-btn color="#a90a56" outlined>ログイン</v-btn>
+    <div>
+        新規登録は<v-btn text color="#a90a56">こちら</v-btn>
+    </div>
+</v-container>
+</template>
+
+<script>
+</script>
+
+<style>
+.container {
+    text-align: center;
+}
+
+.v-image {
+    margin: 0 auto;
+}
+</style>

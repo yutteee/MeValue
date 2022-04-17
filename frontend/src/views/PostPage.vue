69da9b0b97014b412fe6d54d92e88977dd909525
yutteee <yusaku_nakamura_aa@icloud.com>
2022-04-18 01:43:07 +0900

    投稿ページ作った(仮)

---
+++ frontend/src/views/PostPage.vue
@@ -0,0 +1,39 @@
+<template>
+<v-container>
+    <v-card>
+        <h4>評価に協力していただきありがとございます</h4>
+        <h4>評価してほしいコーデをアップロードしてください</h4>
+        <v-card
+            width="250px"
+            elevation="0"
+        >
+            <v-file-input
+                label="image input"
+                filled
+                prepend-icon="mdi-camera"
+            ></v-file-input>
+        </v-card>
+        <h4>コーデ以外をアップロードしないでください</h4>
+        <h4>できるだけ顔が写っていない写真を載せることをお勧めします</h4>
+        <v-btn
+            color="#a90a56"
+            outlined
+        >
+        確定
+        </v-btn>
+    </v-card>
+</v-container>
+</template>
+
+<style scoped>
+.container {
+    text-align: center;
+    margin: 0 auto;
+}
+
+.v-card {
+    text-align: center;
+    margin: 0 auto;
+}
+
+</style>

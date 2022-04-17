2da4b1ddddd9aa7fa3de11ee8fd56d22004c9872
yutteee <yusaku_nakamura_aa@icloud.com>
2022-04-18 01:43:44 +0900

    評価ページ作った

---
+++ frontend/src/views/EvaluationPage.vue
@@ -0,0 +1,27 @@
+<template>
+<v-container>
+    <v-card>
+        <v-img
+            src="../assets/sample.jpeg"
+            max-height="400px"
+            contain
+        />
+        <v-rating
+            v-model="rating"
+            background-color="#a90a56"
+            color="#a90a56"
+            large
+        ></v-rating>
+    </v-card>
+</v-container>
+</template>
+
+<style scoped>
+.container {
+    text-align: center;
+}
+
+.v-image {
+    margin: 0 auto;
+}
+</style>
